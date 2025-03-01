407f69f5f76d46837ad47fa36d712641bc76a154
Abravo10 <abraham_337_@hotmail.com>
2025-03-01 12:18:27 -0500

    Update RED_TX.py

--- pages/RED_TX.py
+++ pages/RED_TX.py
@@ -41,7 +41,11 @@ layout = html.Div([
                      {'label': 'ANILLO', 'value': 'ANILLO'}], ###CAMBIASTE CODIGO por ID
             value='ID', 
             inline=True, 
-            id='type_selection'
+            id='type_selection',
+            className="btn-group",
+            inputClassName="btn-check",
+            labelClassName="btn btn-outline-primary",
+            labelCheckedClassName="active",    
         ),
 
         dcc.Dropdown(
