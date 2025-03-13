6ee5a358a5c2a1ecee332bbe5da8580438635e6b
Abravo10 <abraham_337_@hotmail.com>
2025-03-13 15:07:32 -0500

    Update RED_TX.py

--- pages/RED_TX.py
+++ pages/RED_TX.py
@@ -306,7 +306,7 @@ def update_graph(selected_region, selected_nodos, selected_type):
     if not sum_label:
         sum_label = "Nodo/s sin clientes o IAOs dependientes"
 
-    sum_label = f"{sum_label}  \n\n**NODOS TX:** {n_tx} \n\nNODOS DISTRITALES: {n_dist} \n\n**NODOS AX:** {n_ax} \n\nPRUEBA DE TOTAL {round(((cliente_select/total_cliente)*100),2)}%"
+    sum_label = f"{sum_label}  \n\n**NODOS TX:** {n_tx}\n**NODOS AX:** {n_ax}  \n\nNODOS DISTRITALES: {n_dist} \n\n AFECTACIÓN EN LA RED(%): {round(((cliente_select/total_cliente)*100),2)}%"
 
 
     lst_tx= filtered_df['ID'].tolist()
